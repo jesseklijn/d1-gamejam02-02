08f59d52dcef49c7cf1044e56bdc8997eef98a3b
Jan Velema <velema98@gmail.com>
2016-09-22 12:43:35 +0200

    Player and movement controler

---
+++ GameJam02-03/Assets/Script/CameraControler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = new Vector3(0, player.transform.position.y, -10.8f);
+	}
+}
---
+++ GameJam02-03/Assets/Script/MovementControler.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementControler : MonoBehaviour {
+
+    public float speed;
+    public float thrust;
+    public float minSpeed;
+    public float maxSpeed;
+
+    private Rigidbody rb;
+    float movementX;
+    float movementY;
+    float accelerationY;
+
+    bool upPressed = false;
+    bool downPressed = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            upPressed = true;
+        }
+        else
+        {
+            upPressed = false;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            downPressed = true;
+        }
+        else
+        {
+            downPressed = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (upPressed)
+        {
+            accelerationY = thrust;
+        }
+        else if (downPressed)
+        {
+            accelerationY = -thrust;
+        }
+        else
+        {
+            accelerationY = 0;
+        }
+        if (movementY < minSpeed) { movementY = minSpeed; }
+        if (movementY > maxSpeed) { movementY = maxSpeed; }
+        movementY += accelerationY;
+        movementX = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(movementX * speed, movementY, 0);
+        rb.AddForce(movement);
+
+        
+    }
+}
